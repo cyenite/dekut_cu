84442bdcd4072761b8844703d17807c7e1c0b5a0
Akron254 <aaronrono42@gmail.com>
2021-04-30 19:56:05 +0300

    mpesa payment

--- lib/pages/make_contribution.dart
+++ lib/pages/make_contribution.dart
@@ -1,4 +1,5 @@
 import 'package:dekut_cu/json/contribution_categories.dart';
+import 'package:dekut_cu/services/mpesa_service.dart';
 import 'package:dekut_cu/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -9,10 +10,10 @@ class CreatBudgetPage extends StatefulWidget {
 }
 
 class _CreatBudgetPageState extends State<CreatBudgetPage> {
+  bool _isLoading = false;
   int activeCategory = 0;
-  TextEditingController _budgetName =
-      TextEditingController();
-  TextEditingController _budgetPrice = TextEditingController(text: "00.00");
+  TextEditingController _phoneNumber = TextEditingController();
+  TextEditingController _amount = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,205 +24,234 @@ class _CreatBudgetPageState extends State<CreatBudgetPage> {
 
   Widget getBody() {
     var size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(color: white, boxShadow: [
-              BoxShadow(
-                color: grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3,
-                // changes position of shadow
-              ),
-            ]),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  top: 60, right: 20, left: 20, bottom: 25),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Make Contribution",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: black),
-                      ),
-                      Row(
-                        children: [Icon(AntDesign.search1)],
-                      )
-                    ],
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(color: white, boxShadow: [
+                  BoxShadow(
+                    color: grey.withOpacity(0.01),
+                    spreadRadius: 10,
+                    blurRadius: 3,
+                    // changes position of shadow
                   ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
-            child: Text(
-              "Choose category",
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: black.withOpacity(0.5)),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-                children: List.generate(categories.length, (index) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    activeCategory = index;
-                  });
-                },
+                ]),
                 child: Padding(
                   padding: const EdgeInsets.only(
-                    left: 10,
-                  ),
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      left: 10,
-                    ),
-                    width: 150,
-                    height: 170,
-                    decoration: BoxDecoration(
-                        color: white,
-                        border: Border.all(
-                            width: 2,
-                            color: activeCategory == index
-                                ? primary
-                                : Colors.transparent),
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: grey.withOpacity(0.01),
-                            spreadRadius: 10,
-                            blurRadius: 3,
-                            // changes position of shadow
-                          ),
-                        ]),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 25, right: 25, top: 20, bottom: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      top: 60, right: 20, left: 20, bottom: 25),
+                  child: Column(
+                    children: [
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: grey.withOpacity(0.15)),
-                              child: Center(
-                                child: Image.asset(
-                                  categories[index]['icon'],
-                                  width: 30,
-                                  height: 30,
-                                  fit: BoxFit.contain,
-                                ),
-                              )),
                           Text(
-                            categories[index]['name'],
+                            "Make Contribution",
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: black),
+                          ),
+                          Row(
+                            children: [Icon(AntDesign.search1)],
                           )
                         ],
                       ),
-                    ),
+                    ],
                   ),
                 ),
-              );
-            })),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Contribution info(optional)",
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                child: Text(
+                  "Choose category",
                   style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 13,
-                      color: Color(0xff67727d)),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: black.withOpacity(0.5)),
                 ),
-                TextField(
-                  controller: _budgetName,
-                  cursorColor: black,
-                  style: TextStyle(
-                      fontSize: 17, fontWeight: FontWeight.bold, color: black),
-                  decoration: InputDecoration(
-                      hintText: "Enter additional information",
-                      border: InputBorder.none),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: (size.width - 140),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Enter amount",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 13,
-                                color: Color(0xff67727d)),
-                          ),
-                          TextField(
-                            controller: _budgetPrice,
-                            cursorColor: black,
-                            style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: black),
-                            decoration: InputDecoration(
-                                hintText: "Amount (KSH)",
-                                border: InputBorder.none),
-                          ),
-                        ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                    children: List.generate(categories.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeCategory = index;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
                       ),
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          left: 10,
+                        ),
+                        width: 150,
+                        height: 170,
+                        decoration: BoxDecoration(
+                            color: white,
+                            border: Border.all(
+                                width: 2,
+                                color: activeCategory == index
+                                    ? primary
+                                    : Colors.transparent),
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: grey.withOpacity(0.01),
+                                spreadRadius: 10,
+                                blurRadius: 3,
+                                // changes position of shadow
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 25, right: 25, top: 20, bottom: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: grey.withOpacity(0.15)),
+                                  child: Center(
+                                    child: Image.asset(
+                                      categories[index]['icon'],
+                                      width: 30,
+                                      height: 30,
+                                      fit: BoxFit.contain,
+                                    ),
+                                  )),
+                              Text(
+                                categories[index]['name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                })),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Enter your phone number",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 13,
+                          color: Color(0xff67727d)),
+                    ),
+                    TextField(
+                      controller: _phoneNumber,
+                      cursorColor: black,
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: black),
+                      decoration: InputDecoration(
+                          hintText: "2547********", border: InputBorder.none),
                     ),
                     SizedBox(
-                      width: 20,
+                      height: 20,
                     ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: primary,
-                          borderRadius: BorderRadius.circular(15)),
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: white,
-                      ),
-                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: (size.width - 140),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Enter amount",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                    color: Color(0xff67727d)),
+                              ),
+                              TextField(
+                                controller: _amount,
+                                cursorColor: black,
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: black),
+                                decoration: InputDecoration(
+                                    hintText: "Amount (KSH)",
+                                    border: InputBorder.none),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            //PaymentHelper.stkPush(_phoneNumber.text, _amount.text);
+                            PaymentHelper.mpesa
+                                .lipaNaMpesa(
+                              phoneNumber: _phoneNumber.text,
+                              amount: double.parse(_amount.text),
+                              businessShortCode: "174379",
+                              callbackUrl:
+                                  "http://mpesa-requestbin.herokuapp.com/1kojq2r1",
+                            )
+                                .then((result) {
+                              _isLoading = false;
+                            }).catchError((error) {
+                              _isLoading = false;
+                            });
+                          },
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                color: primary,
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Align(
+          child: _isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
+        )
+      ],
     );
   }
 }
---
+++ lib/services/mpesa_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mpesa/mpesa.dart';
+
+class PaymentHelper {
+  static Future<String> stkPush(String phone, String amount) async {
+    String phoneFormat = phone;
+    final Map<String, dynamic> paymentData = {
+      "amount": amount,
+      "phone": phone,
+      "customerId": "1",
+      "orderId": "Test",
+    };
+    var response = await http.post(
+      'https://us-central1-test-69dc3.cloudfunctions.net/payments/mpesa/request/',
+      body: paymentData,
+      headers: {'apiKey': "CR0wDuC4T3"},
+    );
+
+    print(response.body);
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    return responseData['message'];
+  }
+
+  static Mpesa mpesa = Mpesa(
+    clientKey: "jjjABXY5KtrZFK4xTYicbIe1voCWi6yP",
+    clientSecret: "tBein4KmPI11iMgb",
+    passKey: "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
+    initiatorPassword: "Akron254",
+    environment: "sandbox",
+  );
+}
